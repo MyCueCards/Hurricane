8f8893275d3d49b85bcdc14a1863142d8e23cd8a
Harley Ehrman <harley.ehrman@outlook.com>
2018-05-26 21:21:43 -0400

    Add project files.

---
+++ Hurricane/Hurricane.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Hurricane
+{
+    partial class Hurricane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(341, 161);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(109, 45);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(65, 100);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(280, 25);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Enter a wind speed (in mph)";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(376, 102);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(74, 31);
+            this.txtInput.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblOutput.Location = new System.Drawing.Point(65, 231);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(76, 25);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "label1";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(569, 390);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(219, 48);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit Application";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Hurricane
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Hurricane";
+            this.Text = "Hurricane";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Hurricane/Hurricane.cs
@@ -0,0 +1,70 @@
+﻿/********************************************************************
+ * Assignment from SDEV 240                                         *
+ * Coded by: Harley Ehrman                                          *
+ *                                                                  *
+ * From book:                                                       *
+ * Microsoft Visual C# 2015:                                        *
+ * An Introduction to Object-Oriented Programming 6th Edition       *
+ * by Joyce Farrell                                                 *
+ *                                                                  *
+ * Instructions (Page 186, Exercise 4):                             *
+ * The Saffir-Simpson Hurricane Scale classifies hurricanes into    *
+ * five categories numbered 1 through 5. Write an application       *
+ * named Hurricane that outputs a hurricane’s category based on the * 
+ * user’s input of the wind speed. Category 5 hurricanes have       *
+ * sustained winds of at least 157 miles per hour. The minimum      *
+ * sustained wind speeds for categories 4 through 1 are 130, 111,   *
+ * 96, and 74 miles per hour, respectively. Any storm with winds of *
+ * less than 74 miles per hour is not a hurricane.                  *
+ ********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hurricane
+{
+    public partial class Hurricane : Form
+    {
+        public Hurricane()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //declare constants and variables
+            const int CAT5 = 157, CAT4 = 130, CAT3 = 111, CAT2 = 96, CAT1 = 74;
+            double WindSpeed = Convert.ToDouble(txtInput.Text);
+            string Category;
+
+            //determine hurricane category
+            if (WindSpeed >= CAT5)
+                Category = "a category 5 hurricane.";
+            else if (WindSpeed >= CAT4)
+                Category = "a category 4 hurricane.";
+            else if (WindSpeed >= CAT3)
+                Category = "a category 3 hurricane.";
+            else if (WindSpeed >= CAT2)
+                Category = "a category 2 hurricane.";
+            else if (WindSpeed >= CAT1)
+                Category = "a category 1 hurricane.";
+            else
+                Category = "not a hurricane.";
+
+            //output
+            lblOutput.Text = "A windspeed of " + WindSpeed + "mph is " + Category;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
